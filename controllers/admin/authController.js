e39dad60801cfc9d001c5859d240b7743b139737
Yazdun <yazduntube@gmail.com>
2022-01-14 20:53:28 +0330

    general : made progress

--- controllers/admin/authController.js
+++ controllers/admin/authController.js
@@ -3,20 +3,18 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../../errors");
 
 const loginAdmin = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
-  if (!email || !password)
-    throw new BadRequestError("email and password are required");
+  if (!username || !password)
+    throw new BadRequestError("username and password are required");
 
-  const user = await User.findOne({ email: email.toLowerCase() });
-  if (!user) throw new UnauthenticatedError("email or password is not correct");
-
-  if (!user.isAdmin)
-    throw new UnauthenticatedError("email or password is not correct");
+  const user = await User.findOne({ username });
+  if (!user)
+    throw new UnauthenticatedError("username or password is not correct");
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect)
-    throw new UnauthenticatedError("email or password is not correct");
+    throw new UnauthenticatedError("username or password is not correct");
 
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({ user: { username: user.username }, token });
